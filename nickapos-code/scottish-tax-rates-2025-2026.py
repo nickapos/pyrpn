08b29ef84b1d75542975816cb3c487ef328c1ca9
nickapos <nickapos@oncrete.gr>
2025-11-24 02:28:48 +0000

    adding print commands

--- nickapos-code/scottish-tax-rates-2025-2026.py
+++ nickapos-code/scottish-tax-rates-2025-2026.py
@@ -20,12 +20,12 @@ def s_rates():
   high_rate=0.42
   high_rate_thres=43663
   high_diff=adv_rate_thres-high_rate_thres
-  interm_rate=0.21
-  interm_rate_thres=27492
-  interm_diff=high_rate_thres-interm_rate_thres
+  interim_rate=0.21
+  interim_rate_thres=27492
+  interim_diff=high_rate_thres-interim_rate_thres
   basic_rate=0.2
   basic_rate_thres=15398
-  basic_diff=interm_rate_thres-basic_rate_thres
+  basic_diff=interim_rate_thres-basic_rate_thres
   start_rate=0.19
   start_rate_thres=12571
   start_diff=basic_rate_thres-start_rate_thres
@@ -33,35 +33,71 @@ def s_rates():
   tax_list=[]
   adv_tax=adv_diff*adv_rate
   high_tax=high_diff*high_rate
-  interm_tax=interm_diff*interm_rate
+  interim_tax=interim_diff*interim_rate
   basic_tax=basic_diff*basic_rate
   start_tax=start_diff*start_rate
+  tax_band_header="TaxBa"
+  tax_header="TaxAm"
   if salary >= top_rate_thres:
     top_diff=salary-top_rate_thres
     top_tax=top_diff*top_rate
-    tax=top_tax+adv_tax+high_tax+interm_tax+basic_tax+start_tax
+    tax=top_tax+adv_tax+high_tax+interim_tax+basic_tax+start_tax
+    PRX(tax_band_header)
+    PRX(top_diff)
+    PRX(adv_diff)
+    PRX(high_diff)
+    PRX(interim_diff)
+    PRX(basic_diff)
+    PRX(start_diff)
+    ADV()
+    PRX(tax_header)
+    PRX(top_tax)
+    PRX(adv_tax)
+    PRX(high_tax)
+    PRX(interim_tax)
+    PRX(basic_tax)
+    PRX(start_tax)
   elif salary >= adv_rate_thres:
     adv_diff= salary-adv_rate_thres
     adv_tax=adv_diff*adv_rate
-    tax=adv_tax+high_tax+interm_tax+basic_tax+start_tax
+    tax=adv_tax+high_tax+interim_tax+basic_tax+start_tax
+    PRX(tax_band_header)
+    PRX(adv_diff)
+    PRX(high_diff)
+    PRX(interim_diff)
+    PRX(basic_diff)
+    PRX(start_diff)
+    ADV()
+    PRX(tax_header)
+    PRX(adv_tax)
+    PRX(high_tax)
+    PRX(interim_tax)
+    PRX(basic_tax)
+    PRX(start_tax)
   elif salary >= high_rate_thres:
     high_diff=salary-high_rate_thres
     high_tax=high_diff*high_rate
-    tax=high_tax+interm_tax+basic_tax+start_tax
-  elif salary >= interm_rate_thres:
-    interm_diff=salary-interm_rate_thres
-    interm_tax=interm_diff*interm_rate
-    tax=interm_tax+basic_tax+start_tax
+    tax=high_tax+interim_tax+basic_tax+start_tax
+    PRX(high_diff)
+    PRX(iterim_diff)
+    PRX(basic_diff)
+    PRX(start_diff)
+  elif salary >= interim_rate_thres:
+    interim_diff=salary-interim_rate_thres
+    interim_tax=interim_diff*interim_rate
+    tax=interim_tax+basic_tax+start_tax
+    PRX(interim_diff)
+    PRX(basic_diff)
+    PRX(start_diff)
   elif salary >= basic_rate_thres:
     basic_diff=salary-basic_rate_thres
     basic_tax=basic_diff*basic_rate
     tax=basic_tax+start_tax
+    PRX(basic_diff)
+    PRX(start_diff)
   else:
     start_tax=salary*start_rate
     tax_list.append(start_tax)
     tax=start_tax
-  PRX(top_diff)
-  PRX(adv_diff)
-  PRX(high_diff)
-  PRX(basic_diff)
-  PRX(start_diff)
+    PRX(start_tax)
+  PRX(tax)
